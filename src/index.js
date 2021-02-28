00d35413e8b17e3e7e7916fbbc2085e9a7f882ce
Teodora Zaharieva <zaharieva.teodora@gmail.com>
2021-02-28 20:01:20 +0200

    removed body-parser

--- src/index.js
+++ src/index.js
@@ -1,5 +1,4 @@
 const express = require('express')
-const bodyParser = require('body-parser')
 const morgan = require('morgan')
 const userRouter = require('./user/router.js')
 
@@ -7,7 +6,7 @@ const port = 3000
 const app = express()
 
 app.use(morgan('dev'))
-app.use(bodyParser.json())
+app.use(express.json())
 app.use('/api/users', userRouter)
 
 app.listen(port, () => console.log(`Server listening at port ${port}...`))
