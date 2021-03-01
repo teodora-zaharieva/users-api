c74118457a68f776bfa93cda92a4901e231b06bc
Teodora Zaharieva <zaharieva.teodora@gmail.com>
2021-03-02 01:55:18 +0200

    added signin validator

--- src/validator.js
+++ src/validator.js
@@ -1,9 +1,21 @@
 const validate = (...fields) => (req, res, next) => {
-  const missingFields = fields.filter((field) => !req.body[field]).map((field) => `${field}: is required!`)
+  const missingFields = fields
+    .filter((field) => !req.body[field])
+    .map((field) => `${field}: is required!`)
 
-  missingFields.length > 0 ? res.json({ error: missingFields }) : next()
+  missingFields.length > 0 ? res.status(400).json({ error: missingFields }) : next()
 }
 
 module.exports = {
-  createUser: validate('username', 'password', 'firstName', 'lastName', 'address', 'country', 'email', 'phone')
+  createUser: validate(
+    'username',
+    'password',
+    'firstName',
+    'lastName',
+    'address',
+    'country',
+    'email',
+    'phone'
+  ),
+  signIn: validate('username', 'password')
 }
