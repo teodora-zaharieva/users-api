253bd25b94ca55035df12443143238068dc5327e
Teodora Zaharieva <zaharieva.teodora@gmail.com>
2021-03-02 01:58:04 +0200

    added routes

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const morgan = require('morgan')
-const userRouter = require('./user/router.js')
+const validator = require('./validator')
+const userRouter = require('./user/router')
+const { signIn } = require('./auth')
 
 const port = 3000
 const app = express()
@@ -8,5 +10,9 @@ const app = express()
 app.use(morgan('dev'))
 app.use(express.json())
 app.use('/api/users', userRouter)
+app.post('/api/session', validator.signIn, signIn)
+app.use('*', (_, res) => {
+  res.status(404).json({ error: 'Not found!' })
+})
 
-app.listen(port, () => console.log(`Server listening at port ${port}...`))
+app.listen(port, () => console.log(`Server listening on port ${port}...`))
